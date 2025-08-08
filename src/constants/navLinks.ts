811e3f89aeecfe430a6c45c54b695417ab920865
YogaAlfaRisqi <yogariysqi4@gmail.com>
2025-08-08 17:06:17 +0700

    first commit

---
+++ src/constants/navLinks.ts
@@ -0,0 +1,6 @@
+export const navLinks = [
+  { name: "Who am i?", href: "#whoami"},
+  { name: "Projects", href: "#projects" },
+  { name: "Blog", href: "#blog" },
+  { name: "Contact", href: "#contact" },
+];
